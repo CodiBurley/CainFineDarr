d4ea2d7b576b75d12c44fce4ebce9897fd78fc57
Codi Burley <burleycj@mail.uc.edu>
2015-01-31 16:44:01 -0500

    Periodically collects location

--- app/src/main/java/com/acm/cainfinedarr/MainActivity.java
+++ app/src/main/java/com/acm/cainfinedarr/MainActivity.java
@@ -2,6 +2,7 @@ package com.acm.cainfinedarr;
 
 import android.app.Activity;
 import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,19 +11,26 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import java.text.BreakIterator;
+import java.text.DateFormat;
+import java.util.Date;
 
 
-public class MainActivity extends Activity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks
+        , GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
     GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
+    private Location mCurrentLocation;
+    private LocationRequest mLocationReq;
+    private boolean requestingLocationUpdates = false;
+    private String mLastUpdateTime;
 
     private TextView mLatitudeText;
     private TextView mLongitudeText;
+    private TextView mUpdateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,17 @@ public class MainActivity extends Activity implements
     protected void initVars() {
         mLatitudeText = (TextView) findViewById(R.id.latitude_text);
         mLongitudeText = (TextView) findViewById(R.id.longitude_text);
+        mUpdateTime = (TextView) findViewById(R.id.update_time);
+
+        mLocationReq = new LocationRequest();
+        setLocationRequestParameters(mLocationReq);
+        requestingLocationUpdates = true;
+    }
+
+    protected void setLocationRequestParameters(LocationRequest req) {
+        req.setInterval(1000);
+        req.setInterval(100);
+        req.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
     }
 
     /************** Implemented methods ****************************/
@@ -52,12 +71,16 @@ public class MainActivity extends Activity implements
     public void onConnected(Bundle connectionHint) {
         Toast t = Toast.makeText(this,"onConnected ran", Toast.LENGTH_LONG);
         t.show();
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+        /*mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         if (mLastLocation != null) {
             mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
             mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }*/
+        if(requestingLocationUpdates) {
+            startLocationUpdates();
         }
+
     }
 
     @Override
@@ -70,6 +93,25 @@ public class MainActivity extends Activity implements
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Toast t = Toast.makeText(this, "Connection Failed", Toast.LENGTH_LONG);
         t.show();
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationReq, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUI();
+    }
+
+    private void updateUI() {
+        mLatitudeText.setText(String.valueOf(mCurrentLocation.getLatitude()));
+        mLongitudeText.setText(String.valueOf(mCurrentLocation.getLongitude()));
+        mUpdateTime.setText(mLastUpdateTime);
+
     }
 
     /******* End Implemented Methods ******************************/
@@ -92,5 +134,6 @@ public class MainActivity extends Activity implements
         }
         return super.onOptionsItemSelected(item);
     }
+
 
 }
