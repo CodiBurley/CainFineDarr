29095c25992887451c319d1e1fb1f289126c3a25
Codi Burley <burleycj@mail.uc.edu>
2015-01-31 15:50:06 -0500

    Basic location being accuired

--- app/src/main/java/com/acm/cainfinedarr/MainActivity.java
+++ app/src/main/java/com/acm/cainfinedarr/MainActivity.java
@@ -1,19 +1,78 @@
 package com.acm.cainfinedarr;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.BreakIterator;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
+    private TextView mLatitudeText;
+    private TextView mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.initVars();
+
+        buildGoogleApiClient();
+        mGoogleApiClient.connect();
     }
 
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    protected void initVars() {
+        mLatitudeText = (TextView) findViewById(R.id.latitude_text);
+        mLongitudeText = (TextView) findViewById(R.id.longitude_text);
+    }
+
+    /************** Implemented methods ****************************/
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Toast t = Toast.makeText(this,"onConnected ran", Toast.LENGTH_LONG);
+        t.show();
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast t = Toast.makeText(this,"Connection Suspended",Toast.LENGTH_LONG);
+        t.show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast t = Toast.makeText(this, "Connection Failed", Toast.LENGTH_LONG);
+        t.show();
+    }
+
+    /******* End Implemented Methods ******************************/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,4 +92,5 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
